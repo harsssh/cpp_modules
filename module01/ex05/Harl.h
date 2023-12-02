4d0356fdf128839977aca26f12de27ccbdca6378
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-02 16:02:53 +0900

    done 01-ex05

---
+++ module01/ex05/Harl.h
@@ -0,0 +1,22 @@
+#ifndef CPP_MODULES_HARL_H
+#define CPP_MODULES_HARL_H
+
+#include <string>
+
+class Harl {
+private:
+	static const int LEVELS = 4;
+
+	void debug();
+
+	void info();
+
+	void warning();
+
+	void error();
+
+public:
+	void complain(std::string level);
+};
+
+#endif //CPP_MODULES_HARL_H
