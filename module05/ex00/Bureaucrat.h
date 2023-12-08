1e0c32f3debe6ef22b04ed5b1441268e1899c211
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-08 15:56:22 +0900

    done 05-ex00

---
+++ module05/ex00/Bureaucrat.h
@@ -0,0 +1,50 @@
+#ifndef CPP_MODULES_BUREAUCRAT_H
+#define CPP_MODULES_BUREAUCRAT_H
+
+#include <string>
+#include <ostream>
+
+class Bureaucrat {
+private:
+	static const int maxGrade_ = 1;
+	static const int minGrade_ = 150;
+	const std::string name_;
+	int grade_;
+
+public:
+	class GradeTooHighException : public std::exception {
+		virtual const char *what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception {
+		virtual const char *what() const throw();
+	};
+
+	Bureaucrat(const std::string &name);
+
+	Bureaucrat(const std::string &name, int grade) throw(GradeTooHighException, GradeTooLowException);
+
+	virtual ~Bureaucrat();
+
+	Bureaucrat(const Bureaucrat &other);
+
+	Bureaucrat &operator=(const Bureaucrat &other);
+
+	void setGrade(int grade) throw(GradeTooHighException, GradeTooLowException);
+
+	const std::string &getName() const;
+
+	int getGrade() const;
+
+	static bool isTooHighGrade(int grade);
+
+	static bool isTooLowGrade(int grade);
+
+	void incrementGrade() throw(GradeTooHighException);
+
+	void decrementGrade() throw(GradeTooLowException);
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+#endif //CPP_MODULES_BUREAUCRAT_H
--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -1,4 +1,73 @@
 #include <iostream>
+#include "Bureaucrat.h"
+
+void testNormal() {
+	std::cout << "Create bureaucrat with grade 42" << std::endl;
+	Bureaucrat bureaucrat("B1", 42);
+	std::cout << bureaucrat << std::endl;
+
+	std::cout << "Increment grade" << std::endl;
+	bureaucrat.incrementGrade();
+	std::cout << bureaucrat << std::endl;
+
+	std::cout << "Decrement grade" << std::endl;
+	bureaucrat.decrementGrade();
+	std::cout << bureaucrat << std::endl;
+}
+
+void testTooHighGrade() {
+	std::cout << "Create bureaucrat with grade 0" << std::endl;
+	try {
+		Bureaucrat bureaucrat("B1", 0);
+		std::cout << bureaucrat << std::endl;
+	} catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << "GradeTooHighException caught" << std::endl;
+	}
+
+	// create bureaucrat with grade 1, then increment
+	std::cout << "Create bureaucrat with grade 1" << std::endl;
+	Bureaucrat bureaucrat("B2", 1);
+	std::cout << bureaucrat << std::endl;
+	try {
+		std::cout << "Increment grade" << std::endl;
+		bureaucrat.incrementGrade();
+		std::cout << bureaucrat << std::endl;
+	} catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << "GradeTooHighException caught" << std::endl;
+	}
+}
+
+void testTooLowGrade() {
+	std::cout << "Create bureaucrat with grade 151" << std::endl;
+	try {
+		Bureaucrat bureaucrat("B1", 151);
+		std::cout << bureaucrat << std::endl;
+	} catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << "GradeTooLowException caught" << std::endl;
+	}
+
+	// create bureaucrat with grade 150, then decrement
+	std::cout << "Create bureaucrat with grade 150" << std::endl;
+	Bureaucrat bureaucrat("B2", 150);
+	std::cout << bureaucrat << std::endl;
+	try {
+		std::cout << "Decrement grade" << std::endl;
+		bureaucrat.decrementGrade();
+		std::cout << bureaucrat << std::endl;
+	} catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << "GradeTooLowException caught" << std::endl;
+	}
+}
 
 int main() {
+	std::cout << "[testNormal]" << std::endl;
+	testNormal();
+
+	std::cout << std::endl;
+	std::cout << "[testTooHighGrade]" << std::endl;
+	testTooHighGrade();
+
+	std::cout << std::endl;
+	std::cout << "[testTooLowGrade]" << std::endl;
+	testTooLowGrade();
 }
