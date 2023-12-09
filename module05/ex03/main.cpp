cdcec1425bc749c551e8ab78442ce71cddbc4fd7
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-09 16:11:19 +0900

    done 05-ex03

--- module05/ex03/main.cpp
+++ module05/ex03/main.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
+#include "Bureaucrat.h"
+#include "AForm.h"
+#include "Intern.h"
+
+void createFormAndExecute(const std::string &formName, const std::string &target) {
+	Intern intern;
+	AForm *form = intern.makeForm(formName, target);
+	if (!form)
+		return;
+	std::cout << *form << std::endl;
+
+	std::cout << "Signs and executes form" << std::endl;
+	Bureaucrat bureaucrat("B1", 1);
+	bureaucrat.signForm(*form);
+	bureaucrat.executeForm(*form);
+
+	delete form;
+}
 
 int main() {
+	std::cout << "[Test ShrubberyCreationForm]" << std::endl;
+	createFormAndExecute("shrubbery creation", "F1");
+
+	std::cout << std::endl;
+	std::cout << "[Test RobotomyRequestForm]" << std::endl;
+	createFormAndExecute("robotomy request", "F2");
+
+	std::cout << std::endl;
+	std::cout << "[Test PresidentialPardonForm]" << std::endl;
+	createFormAndExecute("presidential pardon", "F3");
+
+	std::cout << std::endl;
+	std::cout << "[Test Invalid Form]" << std::endl;
+	createFormAndExecute("invalid form", "F4");
 }
