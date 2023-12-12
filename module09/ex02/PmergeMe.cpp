23f74f8b19548fc3f9bb91e19abf1267cf2921c9
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-12 17:45:33 +0900

    09-ex02 in progress

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -1,1 +1,31 @@
 #include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() : vec_(), deq_() {}
+
+PmergeMe::PmergeMe(const PmergeMe &other) : vec_(other.vec_), deq_(other.deq_) {}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &other) {
+	if (this != &other) {
+		vec_ = other.vec_;
+		deq_ = other.deq_;
+	}
+	return *this;
+}
+
+PmergeMe::~PmergeMe() {}
+
+const std::vector<int> &PmergeMe::getVec() const {
+	return vec_;
+}
+
+const std::deque<int> &PmergeMe::getDeq() const {
+	return deq_;
+}
+
+void PmergeMe::sortVector() {
+	vectorMergeInsertionSort(vec_);
+}
+
+void PmergeMe::sortDeque() {
+	dequeMergeInsertionSort(deq_);
+}
--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "PmergeMe.hpp"
 
 int main() {
 }
