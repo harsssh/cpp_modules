87b3056e183c27c9bb59961d10e4efd6430b892b
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-13 06:53:57 +0900

    clear

--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -2,4 +2,12 @@
 #include "PmergeMe.hpp"
 
 int main() {
+	std::vector<int> vec;
+	for (int i = 10; i >= 0; i--)
+		vec.push_back(i);
+	PmergeMe p(vec.begin(), vec.end());
+	p.sortVector();
+	std::cout << "vec: ";
+	for (std::size_t i = 0; i < p.getVec().size(); ++i)
+		std::cout << i << " ";
 }
